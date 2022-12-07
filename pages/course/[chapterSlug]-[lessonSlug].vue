7d05bc7ccf97824bee82018c1dba1f89951c32a7
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2022-12-07 22:02:46 +0100

    1.4 dynamic routes

---
+++ pages/course/[chapterSlug]-[lessonSlug].vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- http://localhost:3000/course/hello-world -->
+    <h2>Lesson</h2>
+    <p>
+        This is a lesson 3
+    </p>
+    <p>
+        Chapter slug: {{ $route.params.chapterSlug }}
+    </p>
+    <p>
+        Lesson slug: {{ $route.params.lessonSlug }}
+    </p>
+</template>
