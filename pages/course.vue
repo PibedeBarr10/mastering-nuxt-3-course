b77378beb6bd2aa49b859a3d8b55abfe682ee3d8
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-02 00:38:00 +0200

    navigating with NuxtLink

--- pages/course.vue
+++ pages/course.vue
@@ -11,11 +11,28 @@
             </div>
     
             <div class="w-full grid grid-cols-4 gap-4">
-                <div class="mr-4 p-8 bg-white rounded-md hidden md:block">
-                    <h3>
+                <div class="md:mr-4 p-8 bg-white rounded-md col-span-4 md:col-span-1">
+                    <h3 class="font-bold mb-4">
                         Chapters
                     </h3>
-                    <div></div>
+                    <div
+                        v-for="chapter in chapters"
+                        :key="chapter.slug"
+                        class="mb-2 flex flex-col"
+                    >
+                        <h4>{{ chapter.title }}</h4>
+                        <NuxtLink
+                            v-for="(lesson, index) in chapter.lessons"
+                            :key="lesson.slug"
+                            :to="`/course/chapter/${chapter.slug}/lesson/${lesson.slug}`"
+                            class="flex flex-row space-x-1 no-underline prose-sm font-normal"
+                        >
+                            <span class="text-gray-500">
+                                {{ index + 1 }}.
+                            </span>
+                            <span>{{ lesson.title }}</span>
+                        </NuxtLink>
+                    </div>
                 </div>
     
                 <div class="p-8 bg-white rounded-md col-span-4 md:col-span-3">
@@ -25,3 +42,7 @@
         </div>
     </div>
 </template>
+
+<script lang="ts" setup>
+const { chapters } = useCourse()
+</script>
