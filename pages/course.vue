c4473c0a9fe0f660e7a3a630204db15a115ad7fa
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-01 18:18:46 +0200

    first commit - getting started, file-based routing and course design with tailwind

---
+++ pages/course.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="bg-gray-100">
+        <div class="container mx-auto flex flex-col items-center h-full min-h-screen">
+            <div class="my-12">
+                <h1 class="text-center font-medium">
+                    Course:
+                    <span class="font-bold">
+                        Mastering Nuxt
+                    </span>
+                </h1>
+            </div>
+    
+            <div class="w-full grid grid-cols-4 gap-4">
+                <div class="mr-4 p-8 bg-white rounded-md hidden md:block">
+                    <h3>
+                        Chapters
+                    </h3>
+                    <div></div>
+                </div>
+    
+                <div class="p-8 bg-white rounded-md col-span-4 md:col-span-3">
+                    <h2>
+                        Lesson
+                    </h2>
+                    <p>
+                        This is a lesson
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
