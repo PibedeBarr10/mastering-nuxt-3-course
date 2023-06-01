f85f796b66665b4058256a9be4e99afa662fda0e
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-01 23:41:33 +0200

    loading in course data

--- pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -1,6 +1,21 @@
 <template>
     <h3 class="font-bold mb-4">Lesson</h3>
     <p class="mb-2">This is a lesson</p>
-    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
-    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+    <p>{{ chapter.title }}</p>
+    <p>{{ lesson.title }}</p>
 </template>
+
+<script setup>
+const course = useCourse()
+const route = useRoute()
+
+const chapter = computed(() => {
+    return course.chapters.find(
+        (chapter) => chapter.slug === route.params.chapterSlug)
+})
+
+const lesson = computed(() => {
+    return chapter.value.lessons.find(
+        (lesson) => lesson.slug === route.params.lessonSlug)
+})
+</script>
