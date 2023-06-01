f76029830c917b51077af04a1397f5841d7e608b
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-01 23:16:25 +0200

    dynamic routes

---
+++ pages/course/chapter/[chapterSlug]/lesson/[lessonSlug].vue
@@ -0,0 +1,6 @@
+<template>
+    <h3 class="font-bold mb-4">Lesson</h3>
+    <p class="mb-2">This is a lesson</p>
+    <p>Chapter slug: {{ $route.params.chapterSlug }}</p>
+    <p>Lesson slug: {{ $route.params.lessonSlug }}</p>
+</template>
--- pages/course/lesson.vue
+++
@@ -1,4 +0,0 @@
-<template>
-    <h3 class="font-bold">Lesson</h3>
-    <p>This is a lesson</p>
-</template>
