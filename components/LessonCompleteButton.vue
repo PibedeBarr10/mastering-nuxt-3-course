38aaad8e1fbeca65c280876120b4d4fc6dd980a2
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-02 22:05:27 +0200

    tracking progress with useState

---
+++ components/LessonCompleteButton.vue
@@ -0,0 +1,31 @@
+<template>
+    <label
+        class="px-4 py-2 rounded-sm font-bold"
+        :class="[ modelValue ? 'bg-green-500 text-white' : 'bg-gray-200' ]"
+    >
+        <input
+            type="checkbox"
+            :value="modelValue"
+            class="hidden"
+            @input="() => $emit('update:modelValue', !modelValue)"
+        />
+        {{ modelValue ? 'Completed!' : 'Mark as complete' }}
+    </label>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    modelValue: {
+        type: Boolean,
+        default: false
+    }
+})
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+::selection {
+    display: none;
+}
+</style>
