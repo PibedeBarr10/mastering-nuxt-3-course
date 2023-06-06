14d377614162b979d7f0564c56da93df2017bd86
PibedeBarr10 <piotrwojtasiewicz8@gmail.com>
2023-06-06 18:10:20 +0200

    lesson endpoint

---
+++ server/courseData.js
@@ -0,0 +1,85 @@
+export default {
+    title: 'TypeScript with Vue.js 3',
+    chapters: [
+        {
+            title: 'Chapter 1',
+            slug: '1-chapter-1',
+            number: 1,
+            lessons: [
+                {
+                    title: 'Introduction to TypeScript',
+                    slug: '1-introduction-to-typescript',
+                    number: 1,
+                    downloadUrl: '',
+                    videoId: 684236333,
+                    text: `
+                        Abc 1
+                        Def 1
+                    `
+                },
+                {
+                    title: 'TypeScript in Vue Components',
+                    slug: '2-typescript-in-vue-components',
+                    number: 2,
+                    downloadUrl: '',
+                    videoId: 12,
+                    text: `
+                        Abc 1
+                        Def 2
+                    `
+                },
+                {
+                    title: 'Chapter summary',
+                    slug: '3-chapter-summary',
+                    number: 3,
+                    downloadUrl: '',
+                    videoId: 686921522,
+                    text: `
+                        Abc 1
+                        Def 3
+                    `
+                }
+            ]
+        },
+        {
+            title: 'Chapter 2',
+            slug: '2-chapter-2',
+            number: 2,
+            lessons: [
+                {
+                    title: 'Advanced TypeScript',
+                    slug: '1-advanced-typescript',
+                    number: 1,
+                    downloadUrl: '',
+                    videoId: 21,
+                    text: `
+                        Abc 2
+                        Def 1
+                    `
+                },
+                {
+                    title: 'Advanced TypeScript in Vue Components',
+                    slug: '2-advanced-typescript-in-vue-components',
+                    number: 2,
+                    downloadUrl: '',
+                    videoId: 22,
+                    text: `
+                        Abc 2
+                        Def 2
+                    `
+                },
+                {
+                    title: 'Chapter summary',
+                    slug: '3-chapter-summary',
+                    number: 3,
+                    downloadUrl: '',
+                    videoId: 23,
+                    text: `
+                        Abc 2
+                        Def 3
+                    `
+                }
+            ]
+        }
+    ]
+}
